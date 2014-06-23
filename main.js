c7f1ba0bb2efb57d10a0ad6b4b1a603fbbdbe7c9
Steve Kauyedauty <skauyedauty@gmail.com>
2014-06-23 13:44:52 -0600

    first commit completed

---
+++ main.js
@@ -0,0 +1,32 @@
+// for(var i=0; i <600 ; i+=100) {
+// 	console.log(i);
+// }
+
+// for(var i=1 ; i <65; i*=2) {
+// 	 console.log(i);
+// }
+
+// for(var i=0 ; i <3; i ++) { 
+// 	for(var z= 0; z<4; z++) {
+// 	console.log(i)
+// }
+// }
+
+// for(var i=0; i <12; i +=2) {
+// 	console.log (i)
+// }
+
+// for(i=3; i< 18; i+=3) {
+// 	console.log(i);
+// }
+
+// for(i = 9; i > -1; i --){
+// 	console.log(i)
+// }
+
+for(i=1; i<4; i++) {
+	for( z=1; z<=3; z++) {
+	console.log(i);
+	
+	}
+}
